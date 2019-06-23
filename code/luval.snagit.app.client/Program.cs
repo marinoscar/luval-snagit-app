06efc7b8786757c7265ce3f535014117e1f76f4b
marinoscar <oscar@marin.cr>
2019-06-22 22:39:13 -0500

    inital commit

---
+++ code/luval.snagit.app.client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.snagit.app.client
+{
+    class Program
+    {
+        private const string _pipeName = "luval-snagit-app";
+        static void Main(string[] args)
+        {
+
+
+            //Client
+            var client = new NamedPipeClientStream(_pipeName);
+            client.Connect(5000);
+            var writer = new StreamWriter(client);
+
+            while (true)
+            {
+                Console.Write("Enter message: ");
+                string input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input)) break;
+                if (client.IsConnected)
+                {
+                    writer.WriteLine(input);
+                    try
+                    {
+                        writer.Flush();
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Server is unavailable");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Server is unavailable");
+                }
+            }
+        }
+    }
+}
---
+++ code/luval.snagit.server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace luval.snagit.server
+{
+    class Program
+    {
+        private const string _pipeName = "luval-snagit-app";
+        private static string _variable = "START";
+        static void Main(string[] args)
+        {
+            StartServer();
+            Task.Delay(1000).Wait();
+
+
+            //Client
+            //var client = new NamedPipeClientStream(_pipeName);
+            //client.Connect();
+            //var reader = new StreamReader(client);
+            while(_variable != "END")
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void StartServer()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                var server = new NamedPipeServerStream(_pipeName);
+                server.WaitForConnection();
+                var reader = new StreamReader(server);
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    _variable = line;
+                    Console.WriteLine("Recieved: {0}", _variable);
+                }
+            });
+        }
+    }
+}
